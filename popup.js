444fec85ebd7889cc83268bd67adba3a0633eeb2
José Carlos Peñuelas Armenta <jose18_carl@hotmail.com>
2019-10-22 06:04:12 -0500

    Hotfix - Added commets to popup code

--- popup.js
+++ popup.js
@@ -1,10 +1,11 @@
 let send_url_button = document.getElementById('send_url_button');
-
+//Adds functionality to the button
 send_url_button.onclick = function(element) {
+    //Get current tab of the button
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         let url = tabs[0].url;
         chrome.extension.getBackgroundPage().console.log("URL: " + url);
-
+        //Send the URL and set the button backgrond color as green if succeded
         chrome.runtime.sendMessage({type: 'sendURL', url: url}, (response) => {
             if(response == 'success') {
                 send_url_button.style.backgroundColor = '#3aa757';
